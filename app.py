f01be129fda51a8c8bc993d766d0b71c76ffb4ff
Ryan Marchand <71299459+ryanamarchand18@users.noreply.github.com>
2023-03-26 15:03:40 -0400

    Update app.py

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    target = os.environ.get('TARGET', 'Team')
+    target = os.environ.get('TARGET', 'World')
     return 'Hello {}!\n'.format(target)
 
 if __name__ == "__main__":
